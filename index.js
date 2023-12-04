ebc6c6b215ad09114b1c7b3fd975bf431264f568
Hosvile <100496294+Hosvile@users.noreply.github.com>
2023-12-04 19:59:47 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+<script>
+(() => {
+	function loadScript(url, callback) {
+		var script = document.createElement("script");
+		script.type = "text/javascript";
+		script.src = url;
+		script.onload = callback;
+		document.head.appendChild(script);
+	}
+	loadScript("https://cdn.work.ink/js/workink-sitescript.js", function() {
+		workink("https://work.ink/1Qza/lpqu4gk0", ["https://pandadevelopment.net",], []);
+	});
+})();
+</script>
